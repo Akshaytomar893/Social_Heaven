201e6a6151966c630a575f2e3c031303746d4936
Akshay Tomar <54436313+Akshaytomar893@users.noreply.github.com>
2020-10-15 21:27:04 +0530

    Add files via upload

--- Social_Heaven/application/views/Home.php
+++ Social_Heaven/application/views/Home.php
@@ -1,40 +1,35 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Monoton&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/styles.css" >
-    <title>Hello, world!</title>
-
-    <style>
-        
-    </style>
-
-  </head>
-  <body >
+<?php 
+  include('header.php');
+  
+?>
     
     <div class="container-fluid ">
-        <div class="jumbotron bg-dark text-center text-white">
+        <div class="jumbotron bg-dark text-center ">
             <h1 class="pt-3 head">
-              <span id="0">S</span>
-              <span  id="1">o</span>
-              <span  id="2">c</span>
-              <span  id="3">i</span>
-              <span id="4">a</span>
-              <span  id="5">l</span>&nbsp;&nbsp;&nbsp;
-              <span  id="6">H</span>
-              <span  id="7">e</span>
-              <span id="8">a</span>
-              <span  id="9">v</span>
-              <span  id="10">e</span>
-              <span  id="11">n</span>
+              <span id="s">S</span>
+              <span  id="o">o</span>
+              <span  id="c">c</span>
+              <span  id="i">i</span>
+              <span id="a">a</span>
+              <span  id="l">l</span>&nbsp;&nbsp;&nbsp;
+              <span  id="h">H</span>
+              <span  id="e">e</span>
+              <span id="a">a</span>
+              <span  id="v">v</span>
+              <span  id="e">e</span>
+              <span  id="n">n</span>
             </h1>
         </div>
+        <?php
+          if($_SESSION['error']!=""){?>
+              <div class="alert alert-danger text-center mt-1" role="alert">
+                <?php echo $_SESSION['error'] ?>
+              </div><?php
+              $_SESSION['error']="";
+          }
+        
+        
+        ?>
         <div class="row ">
             <div class="col-md-1"></div>
             <div class="col-md-5 mt-1 text-center">
@@ -47,22 +42,23 @@
             
             <div class="col-md-6 mt-5 mx-auto">
                 <div class="row justify-content-center">
-                    <form class="mt pt-2 col-md-8 col-10" id="login">
+                    <form class="mt pt-2 col-md-8 col-10" id="login" action="<?php echo base_url() ?>Social_Heaven_Controller/doLogin" method="POST" >
                       <div class="text-center">
                         <img src="<?php echo base_url() ?>assets/img/logo3.jpg" alt="logo" height="100px" width="100px" >
                       </div>
                         <h2>Login</h2><br>
 
                         <div class="form-group " >
-                            <label for="username">Username :</label>
-                            <input id="username" class="form-control " type="text" name="username">
-                            
+                            <label for="mail">Email :</label>
+                            <input id="mail" class="form-control " type="email" name="mail" required>
+                            <p id="mail_error" ></p>
+							
                         </div>
 
                         <div class="form-group " >
                             <label for="password">Password :</label>
-                            <input id="password" class="form-control " type="password" name="password">
-                            
+                            <input id="password" class="form-control " type="password" name="pswrd" required>
+							
                         </div>
 
                         <input type="submit" class="btn btn-danger m-3" value="Login"> 
@@ -93,47 +89,48 @@
           </button>
         </div>
         <div class="modal-body">
-            <form>
+            <form method="POST" action="<?php echo base_url() ?>Social_Heaven_Controller/doSignup">
                 <div class="form-group">
                     <label for="f_name">First Name</label>
-                    <input type="text" class="form-control" id="f_name"  placeholder="Enter first name">
+                    <input type="text" class="form-control" id="f_name"  placeholder="Enter first name" name="f_name" required>
                     
                 </div>
                 <div class="form-group">
                     <label for="l_name">Last Name</label>
-                    <input type="text" class="form-control" id="l_name"  placeholder="Enter last name">
+                    <input type="text" class="form-control" id="l_name"  placeholder="Enter last name" name="l_name" required>
                     
                 </div>
                 <div class="form-group">
                     <label for="dob">Date of birth</label>
-                    <input type="date" class="form-control" id="dob"  >                    
+                    <input type="date" class="form-control" id="dob" name="dob" required>                    
                 </div><br>
                 <div >
                     <label for="gender">Gender :&nbsp;&nbsp;&nbsp;&nbsp; </label>
-                   <input type="radio"  id="gender" name="gender" value="male" > Male&nbsp;&nbsp;
-                   <input type="radio"  id="gender" name="gender" value="female" > Female  &nbsp;&nbsp;
-                   <input type="radio"  id="gender" name="gender" value="custom" > Custom  &nbsp;&nbsp;            
+                   <input type="radio"   name="gender" value="male" required> Male&nbsp;&nbsp;
+                   <input type="radio"   name="gender" value="female" required> Female  &nbsp;&nbsp;
+                   <input type="radio"   name="gender" value="custom" required> Custom  &nbsp;&nbsp;            
                 </div>
                 <br>
                 
                   
                 <div class="form-group">
                   <label for="Email">Email address</label>
-                  <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                  <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email" required>
                   
                 </div>
                 <div class="form-group">
                   <label for="password">Password</label>
-                  <input type="password" class="form-control" id="password" placeholder="Password">
+                  <input type="password" class="form-control" id="signup_password" placeholder="Password" name="password" required>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-success">Sign Up</button>
                 </div>
                 
                
               </form>
         </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-success">Sign Up</button>
-        </div>
+        
       </div>
     </div>
   </div>
@@ -146,6 +143,27 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="<?php echo base_url() ?>assets/javascript/sh_javascript.js" type="text/javascript" ></script>
+    <script>
+      $("#mail").blur(function(){
+
+        $.ajax({
+          url : "<?php echo base_url() ?>Social_Heaven_Controller/validateMail",
+          method : "POST",
+          data : {datapost : $("#mail").val() },
+          success: function(result){
+            $("#mail_error").html(result);
+          }
+
+        });
+
+      });
+
+
+
+
+
+    </script>
   </body>
 </html>
---
+++ Social_Heaven/application/views/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Monoton&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/styles.css" >
+    <title>Social Heaven</title>
+	<link rel="icon" href="<?php echo base_url() ?>assets/img/logo3.ico" type="image/icon type">
+
+    
+
+  </head>
+  <body>
